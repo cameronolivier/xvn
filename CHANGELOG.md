007f960bcc60b541c17e14498ba73adbe2a06ae0
Cameron Olivier <cameronolivier@gmail.com>
2025-10-13 21:16:47 +0200

    chore: bump version to v1.6.0
    - Update version in Cargo.toml, package.json, tests
    - Update CHANGELOG.md with v1.6.0 release date
    
    Files changed:
    - Cargo.toml
    - Cargo.lock
    - package.json
    - tests/cli_test.rs
    - CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [1.5.0] - 2025-10-13
+## [1.6.0] - 2025-10-13
 
 ### Added - Automatic Default Version Return
 
--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -7,7 +7,7 @@ fn test_version_flag() {
     cmd.arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::contains("xvn 1.5.0"));
+        .stdout(predicate::str::contains("xvn 1.6.0"));
 }
 
 #[test]
