1a2cde25e970e13201f0c19741785af7049243eb
Cameron Olivier <cameronolivier@gmail.com>
2025-10-19 23:51:54 +0200

    feat: improve switching output colors
    - Change version switch success text from green to blue for better visual hierarchy
    - Keep green checkmark for success indication
    - Bump version to 2.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.1.1] - 2025-10-19
+
+### 🎨 Visual Improvements
+
+- **Switching Output**: Changed version switch success message text from green to blue for better visual hierarchy
+- **Color Contrast**: Improved readability by using blue for descriptive text while keeping green checkmark
+
 ## [2.1.0] - 2025-10-19
 
 ### ✨ Major Feature: Wizard Redesign
--- homebrew/anvs.rb
+++ homebrew/anvs.rb
@@ -7,10 +7,10 @@ class Anvs < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/olvrcc/anvs/releases/download/v2.1.0/anvs-aarch64-apple-darwin.tar.gz"
-      sha256 "TBD"  # Will be updated after release
+      sha256 "592f45e735382cb80c7fe6d429754cd4f913f4d1b2a8bcfc26ab9fa2823392ad"
     else
       url "https://github.com/olvrcc/anvs/releases/download/v2.1.0/anvs-x86_64-apple-darwin.tar.gz"
-      sha256 "TBD"  # Will be updated after release
+      sha256 "533d754c52bdaa20607dfa80affaade5e851c8a24868325880ff38def604bb17"
     end
   end
 
